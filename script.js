162755c22cce8f5b606c28aea4adda49292cdb6f
melonTMD <159959432+melonTMD@users.noreply.github.com>
2024-10-02 14:10:39 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggle_hide_or_block(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
+
+function openLink(url) {
+    window.location.href = url;
+}
